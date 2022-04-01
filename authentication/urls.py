2948e484005f2f25ba29565f718182e30271f43d
dalbeertws <dalbeer.tws@tekkiwebsolutions.com>
2022-04-01 11:21:44 +0530

    Written our own getting auth token

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from authentication.views import RegisterView
 # from rest_framework_simplejwt.views import TokenRefreshView
+from authentication.views import LoginApiView#Custom getting auth token
 from rest_framework.authtoken import views
 
 urlpatterns = [
     # path('login/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
     # path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', RegisterView.as_view(), name='auth_register'),
-    path('get-token-auth/', views.obtain_auth_token)
+    path('get-token-auth/', views.obtain_auth_token),
+    path('custom-auth-token/',LoginApiView.as_view())#our own getting auth token
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -16,6 +16,33 @@ class RegisterView(generics.CreateAPIView):
         instance = serializer.save()
         instance.set_password(instance.password)
         instance.save()
+
+
+
+from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
+from rest_framework.authentication import TokenAuthentication
+from django.contrib.auth.hashers import check_password
+from rest_framework.permissions import IsAuthenticated
+# Create your views here.
+class LoginApiView( APIView ):
+
+    def post(self, request, *args, **kwargs):
+        user = User.objects.filter(username=request.POST['username']).first()
+
+        if user and check_password( request.POST['password'], user.password ):
+            obj = Token.objects.filter( user_id=user.pk ).first()
+            if not obj:
+                obj = Token.objects.create( user=user )
+            return Response({
+                'token' : obj.key
+            })
+        else:
+            return Response({
+                'error' : "Incorrect login credentials"
+            })
     
    
         
